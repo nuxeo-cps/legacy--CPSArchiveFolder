86e07016b7d2dbf94fec91aac093a43b6418387a
dwyart <devnull@localhost>
2003-12-15 17:12:18 +0000

    Make AF folderish, it is more logical wrt its behavior

--- ArchiveFolder.py
+++ ArchiveFolder.py
@@ -69,6 +69,9 @@ class CPSArchiveFolder(CPSBaseDocument):
     allow_discussion = 0
     security = ClassSecurityInfo()
 
+    isPrincipiaFolderish = 1
+    ## XXX Check is this is ok
+
     _properties = CPSBaseDocument._properties +\
        ( {'id': 'new_window', 'type': 'boolean', 'mode': 'w',
           'label': 'Open in new window'},
